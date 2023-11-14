285333988af5392825ffdb1ecc1dc1ac8aa38470
icebear2n2 <icebear2n2@gmail.com>
2023-11-14 19:07:55 +0900

    feat: 상품 컬렉션 추가

---
+++ src/components/ProductCollections.jsx
@@ -0,0 +1,400 @@
+import { ProductItem } from "./ProductItem";
+import React, { useState, useEffect } from "react";
+import { CircularPagination } from "./Pagination";
+export function ProductCollections() {
+  // 상품 목록을 저장할 상태 변수
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 16; // 페이지당 상품 수 설정
+  // 상품 목록을 가져오는 로직 (예시로 배열로 설정)
+  useEffect(() => {
+    // 상품 데이터를 fetch 혹은 다른 방법으로 가져옵니다
+    const fetchedProducts = new Array(97).fill({});
+    setProducts(fetchedProducts);
+  }, []);
+
+  const totalPages = Math.ceil(products.length / productsPerPage);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct,
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const nextPage = () =>
+    setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev));
+  const prevPage = () => setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev));
+
+  return (
+    <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <section>
+        <header>
+          <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+            Product Collection
+          </h2>
+
+          <p className="mt-4 max-w-md text-gray-500">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+            praesentium cumque iure dicta incidunt est ipsam, officia dolor
+            fugit natus?
+          </p>
+        </header>
+
+        <div className="mt-8 sm:flex sm:items-center sm:justify-between">
+          <div className="block sm:hidden">
+            <button className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
+              <span className="text-sm font-medium"> Filters & Sorting </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-4 w-4 rtl:rotate-180"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="hidden sm:flex sm:gap-4">
+            <div className="relative">
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
+                  <span className="text-sm font-medium"> Availability </span>
+
+                  <span className="transition group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <div className="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
+                  <div className="w-96 rounded border border-gray-200 bg-white">
+                    <header className="flex items-center justify-between p-4">
+                      <span className="text-sm text-gray-700">
+                        {" "}
+                        0 Selected{" "}
+                      </span>
+
+                      <button
+                        type="button"
+                        className="text-sm text-gray-900 underline underline-offset-4"
+                      >
+                        Reset
+                      </button>
+                    </header>
+
+                    <ul className="space-y-1 border-t border-gray-200 p-4">
+                      <li>
+                        <label
+                          htmlFor="FilterInStock"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterInStock"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            In Stock (5+)
+                          </span>
+                        </label>
+                      </li>
+
+                      <li>
+                        <label
+                          htmlFor="FilterPreOrder"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterPreOrder"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Pre Order (3+)
+                          </span>
+                        </label>
+                      </li>
+
+                      <li>
+                        <label
+                          htmlFor="FilterOutOfStock"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterOutOfStock"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Out of Stock (10+)
+                          </span>
+                        </label>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </details>
+            </div>
+
+            <div className="relative">
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
+                  <span className="text-sm font-medium"> Price </span>
+
+                  <span className="transition group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <div className="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
+                  <div className="w-96 rounded border border-gray-200 bg-white">
+                    <header className="flex items-center justify-between p-4">
+                      <span className="text-sm text-gray-700">
+                        The highest price is $600
+                      </span>
+
+                      <button
+                        type="button"
+                        className="text-sm text-gray-900 underline underline-offset-4"
+                      >
+                        Reset
+                      </button>
+                    </header>
+
+                    <div className="border-t border-gray-200 p-4">
+                      <div className="flex justify-between gap-4">
+                        <label
+                          htmlFor="FilterPriceFrom"
+                          className="flex items-center gap-2"
+                        >
+                          <span className="text-sm text-gray-600">$</span>
+
+                          <input
+                            type="number"
+                            id="FilterPriceFrom"
+                            placeholder="From"
+                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                          />
+                        </label>
+
+                        <label
+                          htmlFor="FilterPriceTo"
+                          className="flex items-center gap-2"
+                        >
+                          <span className="text-sm text-gray-600">$</span>
+
+                          <input
+                            type="number"
+                            id="FilterPriceTo"
+                            placeholder="To"
+                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                          />
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </details>
+            </div>
+            <div className="relative">
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
+                  <span className="text-sm font-medium"> Colors </span>
+
+                  <span className="transition group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <div className="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
+                  <div className="w-96 rounded border border-gray-200 bg-white">
+                    <header className="flex items-center justify-between p-4">
+                      <span className="text-sm text-gray-700">
+                        {" "}
+                        0 Selected{" "}
+                      </span>
+
+                      <button
+                        type="button"
+                        className="text-sm text-gray-900 underline underline-offset-4"
+                      >
+                        Reset
+                      </button>
+                    </header>
+
+                    <ul className="space-y-1 border-t border-gray-200 p-4">
+                      <li>
+                        <label
+                          htmlFor="FilterRed"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterRed"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Red
+                          </span>
+                        </label>
+                      </li>
+
+                      <li>
+                        <label
+                          htmlFor="FilterBlue"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterBlue"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Blue
+                          </span>
+                        </label>
+                      </li>
+
+                      <li>
+                        <label
+                          htmlFor="FilterGreen"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterGreen"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Green
+                          </span>
+                        </label>
+                      </li>
+                      <li>
+                        <label
+                          htmlFor="FilterOrange"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterOrange"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Orange
+                          </span>
+                        </label>
+                      </li>
+                      <li>
+                        <label
+                          htmlFor="FilterPurple"
+                          className="inline-flex items-center gap-2"
+                        >
+                          <input
+                            type="checkbox"
+                            id="FilterPurple"
+                            className="h-5 w-5 rounded border-gray-300"
+                          />
+
+                          <span className="text-sm font-medium text-gray-700">
+                            Purple
+                          </span>
+                        </label>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </details>
+            </div>
+          </div>
+
+          <div className="hidden sm:block">
+            <label htmlFor="SortBy" className="sr-only">
+              SortBy
+            </label>
+
+            <select
+              id="SortBy"
+              className="h-10 rounded border-gray-300 text-sm"
+            >
+              <option>Sort By</option>
+              <option value="Title, DESC">Title, DESC</option>
+              <option value="Title, ASC">Title, ASC</option>
+              <option value="Price, DESC">Price, DESC</option>
+              <option value="Price, ASC">Price, ASC</option>
+            </select>
+          </div>
+        </div>
+
+        <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          {currentProducts.map((product, index) => (
+            <ProductItem key={index} {...product} />
+          ))}
+        </ul>
+        <div className="pagination-container">
+          <CircularPagination
+            active={currentPage}
+            totalPages={totalPages}
+            onPaginate={paginate}
+            onNext={nextPage}
+            onPrev={prevPage}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
