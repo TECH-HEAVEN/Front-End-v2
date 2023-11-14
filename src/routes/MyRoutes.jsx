2d2cb4ffff05b69ce52981144e290954477a9d9a
icebear2n2 <icebear2n2@gmail.com>
2023-11-14 19:09:09 +0900

    feat: 상품 페이지 라우터 추가

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Template from "../templates/Template";
 import Main from "../page/Main";
+import Product from "../page/Product";
 
 const MyRoutes = () => {
   return (
     <Routes>
       <Route element={<Template />}>
         <Route path="/" element={<Main numCards={5} />} />
+        <Route path="/product" element={<Product />} />
       </Route>
     </Routes>
   );
