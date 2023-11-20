8f51ea043d3b0158a0b9f612f28b13542a2879cb
icebear2n2 <icebear2n2@gmail.com>
2023-11-20 19:16:49 +0900

    fix: 설정 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,6 +5,7 @@ import { FeaturedProduct } from "../components/FeaturedProduct";
 import { CollectionProduct } from "../components/CollectionProduct";
 import React, { useState } from "react";
 import { Timer } from "../components/Timer";
+import { Button } from "@material-tailwind/react";
 const Main = ({ numCards }) => {
   // // CollectionCard 컴포넌트의 배열을 생성합니다.
   // const cards = Array.from({ length: numCards }, (_, index) => (
@@ -32,7 +33,36 @@ const Main = ({ numCards }) => {
     <>
       <div className="bg-white">
         <div className="mx-auto max-w-screen-2xl px-4 pb-6 pt-16 sm:px-6 lg:px-8">
-          <Timer />
+          <header>
+            <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Time Sale
+            </h2>
+            <p className="mt-4 max-w-md text-gray-500 mb-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+              praesentium cumque iure dicta incidunt est ipsam, officia dolor
+              fugit natus?
+            </p>
+          </header>
+          <div className="flex flex-row items-start justify-between">
+            <Timer />
+            <Button className="flex flex items-center mt-auto gap-2">
+              Read More{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </Button>
+          </div>
           <div className="collection-card-container">{currentCards}</div>
 
           <div className="pagination-container">
